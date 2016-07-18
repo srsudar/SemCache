a41890854e6f99db508d45ca2f208bdd69d87f13
Sam Sudar <sudar.sam@gmail.com>
2016-07-18 15:42:13 +0100

    Fix test for dns-sd-semcache

--- chromeapp/test/scripts/dnssd/dns-sd-semcache-test.js
+++ chromeapp/test/scripts/dnssd/dns-sd-semcache-test.js
@@ -10,15 +10,17 @@ test('registerSemCache calls dnssd.register with correct args', function(t) {
     './dns-sd': {register: registerMock}
   });
 
+  var host = 'workstation.local';
   var name = 'Fancy SemCache';
   var port = 1234;
 
-  dnssdSem.registerSemCache(name, port);
+  dnssdSem.registerSemCache(host, name, port);
 
   // Verify register was called with the correct arguments.
-  t.equal(registerMock.firstCall.args[0], name);
-  t.equal(registerMock.firstCall.args[1], dnssdSem.getSemCacheServiceString());
-  t.equal(registerMock.firstCall.args[2], port);
+  t.equal(registerMock.firstCall.args[0], host);
+  t.equal(registerMock.firstCall.args[1], name);
+  t.equal(registerMock.firstCall.args[2], dnssdSem.getSemCacheServiceString());
+  t.equal(registerMock.firstCall.args[3], port);
 
   t.end();
 });
@@ -41,7 +43,7 @@ test('browseForSemCacheInstances calls browse with correct args', function(t) {
   // This function should just call through to dns-sd.
   var browseMock = sinon.spy();
   var dnssdSem = proxyquire('../../../app/scripts/dnssd/dns-sd-semcache', {
-    './dns-sd': {browse: browseMock}
+    './dns-sd': { browseServiceInstances: browseMock }
   });
 
   dnssdSem.browseForSemCacheInstances();
@@ -57,7 +59,7 @@ test('browseForSemCacheInstances returns dnssd.browse result', function(t) {
   var returnResult = 'manyinstances';
   var browseMock = sinon.stub().returns(returnResult);
   var dnssdSem = proxyquire('../../../app/scripts/dnssd/dns-sd-semcache', {
-    './dns-sd': {browse: browseMock}
+    './dns-sd': { browseServiceInstances: browseMock }
   });
 
   var actualReturn = dnssdSem.browseForSemCacheInstances();
