3b9e75b73e3b277ac7928e554e92b9ce10e94fe0
Sam Sudar <sudar.sam@gmail.com>
2016-08-22 11:31:10 -0700

    Add datastore module for code review

---
+++ chromeapp/app/scripts/persistence/datastore.js
@@ -0,0 +1,213 @@
+/* globals Promise */
+'use strict';
+
+/**
+ * Abstractions for reading and writing cached pages. Clients of this class
+ * should not be concerned with the underlying file system.
+ */
+
+// Overview of the Datastore
+//
+// For the time being, there is no separate database or datastore. All
+// information is saved in the file name on disk, eg
+// "www.example.com_date". This will serve for a prototype but might become
+// limiting in the future.
+
+var fileSystem = require('./file-system');
+var fsUtil = require('./file-system-util');
+var serverApi = require('../server/server-api');
+
+/** The number of characters output by Date.toISOString() */
+var LENGTH_ISO_DATE_STR = 24;
+
+var URL_DATE_DELIMITER = '_';
+
+exports.MHTML_EXTENSION = '.mhtml';
+
+/**
+ * This object represents a page that is stored in the cache and can be browsed
+ * to.
+ *
+ * @param {string} captureUrl the URL of the original captured page
+ * @param {string} captureDate the ISO String representation of the datetime
+ * @param {string} accessPath the path in the cache that can be used to access
+ * the file the page was captured
+ */
+exports.CachedPage = function CachedPage(
+  captureUrl,
+  captureDate,
+  path
+) {
+  if (!(this instanceof CachedPage)) {
+    throw new Error('CachedPage must be called with new');
+  }
+  this.captureUrl = captureUrl;
+  this.captureDate = captureDate;
+  this.accessPath = path;
+};
+
+/**
+ * Write a page into the cache.
+ *
+ * @param {string} captureUrl the URL that generated the MHTML
+ * @param {string} captureDate the toISOString() of the date the page was
+ * captured
+ * @param {Blob} mhtmlBlob the contents of hte page
+ *
+ * @return {Promise} a Promise that resolves when the write is complete
+ */
+exports.addPageToCache = function(captureUrl, captureDate, mhtmlBlob) {
+  return new Promise(function(resolve, reject) {
+    // Get the directory to write into
+    // Create the file entry
+    // Perform the write
+    var heldEntry = null;
+    fileSystem.getDirectoryForCacheEntries()
+    .then(cacheDir => {
+      var fileName = exports.createFileNameForPage(captureUrl, captureDate);
+      var createOptions = {
+        create: true,     // create if it doesn't exist
+        exclusive: false  // OK if it already exists--will overwrite
+      };
+      return fsUtil.getFile(cacheDir, createOptions, fileName);
+    })
+    .then(fileEntry => {
+      heldEntry = fileEntry;
+      return fsUtil.writeToFile(fileEntry, mhtmlBlob);
+    })
+    .then(() => {
+      resolve(heldEntry);
+    })
+    .catch(err => {
+      reject(err);
+    });
+  });
+};
+
+/**
+ * Get all the cached pages that are stored in the cache.
+ *
+ * @return {Promise} Promise that resolves with an Array of CachedPage objects
+ */
+exports.getAllCachedPages = function() {
+  return new Promise(function(resolve, reject) {
+    exports.getAllFileEntriesForPages()
+    .then(entries => {
+      var result = [];
+      entries.forEach(entry => {
+        var cachedPage = exports.getEntryAsCachedPage(entry);
+        result.push(cachedPage);
+      });
+      resolve(result);
+    })
+    .catch(err => {
+      reject(err);
+    });
+  });
+};
+
+/**
+ * Get all the FileEntries representing saved pages.
+ *
+ * @return {Promise} Promise that resolves with an array of FileEntry objects
+ */
+exports.getAllFileEntriesForPages = function() {
+  var flagDirNotSet = 1;
+  return new Promise(function(resolve, reject) {
+    fileSystem.getDirectoryForCacheEntries()
+    .then(dirEntry => {
+      if (!dirEntry) {
+        // We haven't set an entry.
+        throw flagDirNotSet;
+      }
+      return fsUtil.listEntries(dirEntry);
+    })
+    .then(entries => {
+      resolve(entries);
+    })
+    .catch(errFlag => {
+      if (errFlag === flagDirNotSet) {
+        reject('dir not set');
+      } else {
+        console.warn('unrecognized error flag: ', errFlag);
+      }
+    });
+  });
+};
+
+/**
+ * Convert an entry as represented on the file system to a CachedPage that can
+ * be consumed by clients.
+ *
+ * This is the workhorse function for mapping between the two types.
+ *
+ * @param {FileEntry} entry
+ *
+ * @return {CachedPage}
+ */
+exports.getEntryAsCachedPage = function(entry) {
+  var captureUrl = exports.getCaptureUrlFromName(entry.name);
+  var captureDate = exports.getCaptureDateFromName(entry.name);
+  var accessUrl = serverApi.getAccessUrlForCachedPage(entry.fullPath);
+
+  var result = new exports.CachedPage(captureUrl, captureDate, accessUrl);
+  return result;
+};
+
+/**
+ * Create the file name for the cached page in a way that can later be parsed.
+ *
+ * @param {string} captureUrl
+ * @param {string} captureDate the toISOString() representation of the date the
+ * page was captured
+ *
+ * @return {string}
+ */
+exports.createFileNameForPage = function(captureUrl, captureDate) {
+  return captureUrl +
+    URL_DATE_DELIMITER +
+    captureDate +
+    exports.MHTML_EXTENSION;
+};
+
+/**
+ * @param {string} name the name of the file
+ *
+ * @return {string} the capture url
+ */
+exports.getCaptureUrlFromName = function(name) {
+  var nonNameLength = LENGTH_ISO_DATE_STR +
+    URL_DATE_DELIMITER.length +
+    exports.MHTML_EXTENSION.length;
+  if (name.length < nonNameLength) {
+    // The file name is too short, fail fast.
+    throw new Error('name too short to store a url: ', name);
+  }
+
+  var result = name.substring(
+    0,
+    name.length - nonNameLength
+  );
+  return result;
+};
+
+/**
+ * @param {string} name the name of the file
+ * 
+ * @return {string} the capture date's ISO string representation
+ */
+exports.getCaptureDateFromName = function(name) {
+  // The date is stored at the end of the string.
+  if (name.length < LENGTH_ISO_DATE_STR) {
+    // We've violated an invariant, fail fast.
+    throw new Error('name too short to store a date: ', name);
+  }
+
+  var dateStartIndex = name.length -
+    LENGTH_ISO_DATE_STR -
+    exports.MHTML_EXTENSION.length;
+  var dateEndIndex = name.length - exports.MHTML_EXTENSION.length;
+
+  var result = name.substring(dateStartIndex, dateEndIndex);
+  return result;
+};
---
+++ chromeapp/test/scripts/persistence/datastore-test.js
@@ -0,0 +1,300 @@
+'use strict';
+var test = require('tape');
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+require('sinon-as-promised');
+
+var datastore = require('../../../app/scripts/persistence/datastore');
+
+/**
+ * Manipulating the object directly leads to polluting the require cache. Any
+ * test that modifies the required object should call this method to get a
+ * fresh version
+ */
+function resetDatastore() {
+  delete require.cache[
+    require.resolve('../../../app/scripts/persistence/datastore')
+  ];
+  datastore = require('../../../app/scripts/persistence/datastore');
+}
+
+test('CachedPage constructs', function(t) {
+  var url = 'http://www.example.com';
+  var path = 'pages/www.example.com';
+  var captureDate = 'date';
+
+  var actual = new datastore.CachedPage(url, captureDate, path);
+
+  t.equal(actual.captureUrl, url);
+  t.equal(actual.captureDate, captureDate);
+  t.equal(actual.accessPath, path);
+
+  t.end();
+});
+
+test('createFileNameForPage returns correct string', function(t) {
+  var url = 'http://whatever-you-say.url.org.co.lt';
+  var captureDate = '2016-07-22T08:49:19.182Z';
+
+  var actual = datastore.createFileNameForPage(url, captureDate);
+  var expected = url + '_' + captureDate + '.mhtml';
+
+  t.equal(expected, actual);
+  t.end();
+});
+
+test('getCaptureUrlFromName returns name', function(t) {
+  var expected = 'www.example.com/hello_world.html';
+  var name = expected + '_2016-07-22T08:49:19.182Z.mhtml';
+  var actual = datastore.getCaptureUrlFromName(name);
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('getCaptureDateFromName returns date string', function(t) {
+  var expected = '2016-07-22T08:49:19.182Z';
+  var name = 'www.example.com/hello_world.html_' + expected + '.mhtml';
+  var actual = datastore.getCaptureDateFromName(name);
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('getAllCachedPages resolves all pages', function(t) {
+  var entries = ['a', 'b', 3, {}];
+  var getAllFileEntriesSpy = sinon.stub().resolves(entries);
+
+  var getMockCachedPage = function(param) {
+    return { cachedPage: param };
+  };
+  var expectedCachedPages = [];
+  entries.forEach(entry => {
+    expectedCachedPages.push(getMockCachedPage(entry));
+  });
+
+  var getEntryAsCachedPageSpy = sinon.stub();
+  for (var i = 0; i < expectedCachedPages.length; i++) {
+    getEntryAsCachedPageSpy.onCall(i).returns(expectedCachedPages[i]);
+  }
+
+  datastore.getAllFileEntriesForPages = getAllFileEntriesSpy;
+  datastore.getEntryAsCachedPage = getEntryAsCachedPageSpy;
+
+  datastore.getAllCachedPages()
+  .then(pages => {
+    t.deepEqual(pages, expectedCachedPages);
+    t.end();
+    resetDatastore();
+  });
+});
+
+test('getAllCachedPages rejects if base dir not set', function(t) {
+  var expectedErr = {cause: 'error msg from getAllFileEntriesForPages'};
+  var getAllFileEntriesSpy = sinon.stub().rejects(expectedErr);
+
+  datastore.getAllFileEntriesForPages = getAllFileEntriesSpy;
+
+  datastore.getAllCachedPages()
+  .catch(actualErr => {
+    t.equal(actualErr, expectedErr);
+    t.end();
+    resetDatastore();
+  });
+});
+
+test('getAllFileEntriesForPages resolves all pages', function(t) {
+  var dirEntry = 'cacheDir';
+  var getDirectoryForCacheEntriesSpy = sinon.stub().resolves(dirEntry);
+
+  var expectedEntries = ['a', 'b', 54321, 'foobar', {}];
+  var listEntriesSpy = sinon.stub().resolves(expectedEntries);
+
+  datastore = proxyquire(
+    '../../../app/scripts/persistence/datastore',
+    {
+      './file-system': {
+        getDirectoryForCacheEntries: getDirectoryForCacheEntriesSpy
+      },
+      './file-system-util': {
+        listEntries: listEntriesSpy
+      }
+    }
+  );
+
+  datastore.getAllFileEntriesForPages()
+  .then(entries => {
+    t.deepEqual(entries, expectedEntries);
+    t.end();
+    resetDatastore();
+  });
+});
+
+test('getAllFileEntriesForPages rejects if base dir not set', function(t) {
+  datastore = proxyquire(
+    '../../../app/scripts/persistence/datastore',
+    {
+      './file-system': {
+        getDirectoryForCacheEntries: sinon.stub().resolves(null)
+      }
+    }
+  );
+
+  datastore.getAllFileEntriesForPages()
+  .catch(err => {
+    t.equal(err, 'dir not set');
+    t.end();
+    resetDatastore();
+  });
+});
+
+test('getEntryAsCachedPage returns CachedPage', function(t) {
+  var url = 'www.example.co.uk/fancyExample.html';
+  var date = 'dateTime';
+  var entry = {
+    name: url + '_' + date,
+    fullPath: '/cache/dir/www.example.co.uk/fancyExample.html_dateTime',
+  };
+
+  var accessUrl = 'the url with the file';
+  var getAccessUrlStub = sinon.stub().returns(accessUrl);
+
+  datastore = proxyquire(
+    '../../../app/scripts/persistence/datastore',
+    {
+      '../server/server-api': {
+        getAccessUrlForCachedPage: getAccessUrlStub
+      }
+    }
+  );
+
+  var getUrlStub = sinon.stub().returns(url);
+  var getDateStub = sinon.stub().returns(date);
+
+  datastore.getCaptureUrlFromName = getUrlStub;
+  datastore.getCaptureDateFromName = getDateStub;
+
+  var expected = new datastore.CachedPage(url, date, accessUrl);
+  var actual = datastore.getEntryAsCachedPage(entry);
+
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test(
+  'addPageToCache rejects if getDirectoryForCacheEntries rejects',
+  function(t) {
+    var errObj = { msg: 'no base dir' };
+    var getDirectoryForCacheEntriesSpy = sinon.stub().rejects(errObj);
+
+    datastore = proxyquire(
+      '../../../app/scripts/persistence/datastore',
+      {
+        './file-system': {
+          getDirectoryForCacheEntries: getDirectoryForCacheEntriesSpy
+        }
+      }
+    );
+
+    datastore.addPageToCache('url', 'date', 'blob')
+      .catch(err => {
+        t.equal(err, errObj);
+        t.end();
+        resetDatastore();
+      });
+  }
+);
+
+test('addPageToCache rejects if getFile rejects', function(t) {
+  var errObj = { msg: 'no base dir' };
+  var getDirectoryForCacheEntriesSpy = sinon.stub().rejects(errObj);
+  var getFileSpy = sinon.stub().rejects(errObj);
+
+  datastore = proxyquire(
+    '../../../app/scripts/persistence/datastore',
+    {
+      './file-system': {
+        getDirectoryForCacheEntries: getDirectoryForCacheEntriesSpy
+      },
+      './file-system-util': {
+        getFile: getFileSpy
+      }
+    }
+  );
+
+  datastore.addPageToCache('url', 'date', 'blob')
+  .catch(err => {
+    t.equal(err, errObj);
+    t.end();
+    resetDatastore();
+  });
+});
+
+test('addPageToCache rejects if writeToFile rejects', function(t) {
+  var errObj = { msg: 'no base dir' };
+  var getDirectoryForCacheEntriesSpy = sinon.stub().rejects(errObj);
+  var getFileSpy = sinon.stub().resolves();
+  var writeToFileSpy = sinon.stub().rejects(errObj);
+
+  datastore = proxyquire(
+    '../../../app/scripts/persistence/datastore',
+    {
+      './file-system': {
+        getDirectoryForCacheEntries: getDirectoryForCacheEntriesSpy
+      },
+      './file-system-util': {
+        getFile: getFileSpy,
+        writeToFile: writeToFileSpy
+      }
+    }
+  );
+
+  datastore.addPageToCache('url', 'date', 'blob')
+  .catch(err => {
+    t.equal(err, errObj);
+    t.end();
+    resetDatastore();
+  });
+});
+
+test('addPageToCache resolves if all others succeed', function(t) {
+  var captureUrl = 'http://www.example.com/hilarious/kitty/cats.html';
+  var captureDate = 'today';
+  var blob = {much: 'blob'};
+  var fileName = 'file_entry_name.mhtml';
+  var dirEntryStub = {cacheDir: 'someDir'};
+  var fileEntryStub = {fileName: 'sofancy'};
+  var getDirectoryForCacheEntriesSpy = sinon.stub().resolves(dirEntryStub);
+  var getFileSpy = sinon.stub().resolves(fileEntryStub);
+  var writeToFileSpy = sinon.stub().resolves();
+
+  datastore = proxyquire(
+    '../../../app/scripts/persistence/datastore',
+    {
+      './file-system': {
+        getDirectoryForCacheEntries: getDirectoryForCacheEntriesSpy
+      },
+      './file-system-util': {
+        getFile: getFileSpy,
+        writeToFile: writeToFileSpy
+      }
+    }
+  );
+  var createFileNameSpy = sinon.stub().returns(fileName);
+  datastore.createFileNameForPage = createFileNameSpy;
+
+  datastore.addPageToCache(captureUrl, captureDate, blob)
+  .then(returnedFile => {
+    t.deepEqual(returnedFile, fileEntryStub);
+    t.deepEqual(createFileNameSpy.args[0], [captureUrl, captureDate]);
+    t.deepEqual(getFileSpy.args[0],
+      [
+        dirEntryStub, {create: true, exclusive: false},
+        fileName
+      ]
+    );
+    t.deepEqual(writeToFileSpy.args[0], [fileEntryStub, blob]);
+    t.end();
+    resetDatastore();
+  });
+});
