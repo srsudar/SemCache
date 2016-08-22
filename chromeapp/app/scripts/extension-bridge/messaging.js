0b309b4199a41ccee726115c63f22a448d0cf7e6
Sam Sudar <sudar.sam@gmail.com>
2016-08-22 11:26:46 -0700

    Add extension-bridge module for code review

---
+++ chromeapp/app/scripts/extension-bridge/messaging.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var chromeRuntime = require('../chrome-apis/runtime');
+var datastore = require('../persistence/datastore');
+var base64 = require('base-64');
+
+/**
+ * ID of the Semcache extension.
+ */
+exports.EXTENSION_ID = 'malgfdapbefeeidjfndgioclhfpfglhe';
+
+/**
+ * Send a message to the extension.
+ *
+ * @param {any} message
+ */
+exports.sendMessageToExtension = function(message) {
+  chromeRuntime.sendMessage(exports.EXTENSION_ID, message);
+};
+
+/**
+ * Function to handle messages coming from the SemCache extension.
+ *
+ * @param {object} message message sent by the extension. Expected to have the
+ * following format:
+ * {
+ *   type: 'write'
+ *   params: {captureUrl: 'url', captureDate: 'iso', dataUrl: 'string'}
+ * }
+ * @param {MessageSender}
+ * @param {function}
+ */
+exports.handleExternalMessage = function(message, sender, response) {
+  if (sender.id !== exports.EXTENSION_ID) {
+    console.log('ID not from SemCache extension: ', sender);
+    return;
+  }
+  if (message.type === 'write') {
+    var blob = exports.getBlobFromDataUrl(message.params.dataUrl);
+    var captureUrl = message.params.captureUrl;
+    var captureDate = message.params.captureDate;
+    datastore.addPageToCache(captureUrl, captureDate, blob);
+    if (response) {
+      response();
+    }
+  } else {
+    console.log('Unrecognized message type from extension: ', message.type);
+  }
+};
+
+/**
+ * @param {string} dataUrl a data url as encoded by FileReader.readAsDataURL
+ *
+ * @return {Blob}
+ */
+exports.getBlobFromDataUrl = function(dataUrl) {
+  // Decoding from data URL based on:
+  // https://gist.github.com/fupslot/5015897
+  var byteString = base64.decode(dataUrl.split(',')[1]);
+  var mime = dataUrl.split(',')[0].split(':')[1].split(';')[0];
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+  var ia = new Uint8Array(ab);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  // write the ArrayBuffer to a blob, and you're done
+  var result = new Blob([ab], {type: mime});
+  return result;
+};
+
+exports.attachListeners = function() {
+  chromeRuntime.addOnMessageExternalListener(exports.handleExternalMessage);
+};
+
+/**
+ * Send a message to the Extension instructing it to open the URL.
+ *
+ * @param {string} url
+ */
+exports.sendMessageToOpenUrl = function(url) {
+  var message = {
+    type: 'open',
+    params: {
+      url: url
+    }
+  };
+  exports.sendMessageToExtension(message);
+};
---
+++ chromeapp/test/scripts/extension-bridge/messaging-test.js
@@ -0,0 +1,104 @@
+'use strict';
+var test = require('tape');
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+require('sinon-as-promised');
+
+/**
+ * Manipulating the object directly leads to polluting the require cache. Any
+ * test that modifies the required object should call this method to get a
+ * fresh version
+ */
+function resetMessaging() {
+  delete require.cache[
+    require.resolve('../../../app/scripts/extension-bridge/messaging')
+  ];
+}
+
+/**
+ * Return a MessageSender object from our extension.
+ */
+function getSender() {
+  var messaging = require('../../../app/scripts/extension-bridge/messaging');
+  var extensionId = messaging.EXTENSION_ID;
+  delete require.cache[
+    require.resolve('../../../app/scripts/extension-bridge/messaging')
+  ];
+  return { id: extensionId };
+
+}
+
+test('handleExternalMessage adds page to cache for write', function(t) {
+  var params = {
+    captureUrl: 'www.example.com',
+    captureData: 'some day',
+    dataUrl: 'data:base64'
+  };
+  var message = {
+    type: 'write',
+    params: params
+  };
+  var addPageToCacheSpy = sinon.spy();
+
+  var messaging = proxyquire(
+    '../../../app/scripts/extension-bridge/messaging',
+    {
+      '../persistence/datastore': {
+        addPageToCache: addPageToCacheSpy
+      }
+    }
+  );
+  var blob = {binary: '101s', type: 'mhtml'};
+  var getBlobFromDataUrlSpy = sinon.stub().returns(blob);
+  messaging.getBlobFromDataUrl = getBlobFromDataUrlSpy;
+
+  var sender = getSender();
+  messaging.handleExternalMessage(message, sender);
+
+  t.deepEqual(addPageToCacheSpy.args[0],
+    [
+      params.captureUrl,
+      params.captureDate,
+      blob
+    ]
+  );
+  t.end();
+});
+
+test('sendMessageToExtension calls sendMessage', function(t) {
+  var sendMessageSpy = sinon.spy();
+  var messaging = proxyquire('../../../app/scripts/extension-bridge/messaging',
+    {
+      '../chrome-apis/runtime': {
+        sendMessage: sendMessageSpy
+      }
+    }
+  );
+
+  var message = {hello: 'big fella'};
+
+  messaging.sendMessageToExtension(message);
+  t.equal(sendMessageSpy.args[0][0], messaging.EXTENSION_ID);
+  t.deepEqual(sendMessageSpy.args[0][1], message);
+  t.end();
+  resetMessaging();
+});
+
+test('sendMessageToOpenUrl sends correct message', function(t) {
+  var url = 'open me plz';
+  var expectedMessage = {
+    type: 'open',
+    params: {
+      url: url
+    }
+  };
+
+  var messaging = require('../../../app/scripts/extension-bridge/messaging');
+  var sendMessageToExtensionSpy = sinon.spy();
+  messaging.sendMessageToExtension = sendMessageToExtensionSpy;
+  messaging.sendMessageToOpenUrl(url);
+
+  t.deepEqual(sendMessageToExtensionSpy.args[0][0], expectedMessage);
+  t.end();
+  resetMessaging();
+});
