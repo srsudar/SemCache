dc33094e2a825cc6391e9d05b95133eebe76239d
Sam Sudar <sudar.sam@gmail.com>
2016-08-25 14:55:25 -0700

    Add changes from storemetadata branch
    Allows metadata to be passed and written to the datastore,
    enabling saving jpeg snapshots, favicons, etc along with the
    MHTML file itself.

--- chromeapp/test/scripts/extension-bridge/messaging-test.js
+++ chromeapp/test/scripts/extension-bridge/messaging-test.js
@@ -3,6 +3,7 @@ var test = require('tape');
 var sinon = require('sinon');
 var proxyquire = require('proxyquire');
 require('sinon-as-promised');
+var messaging = require('../../../app/scripts/extension-bridge/messaging');
 
 /**
  * Manipulating the object directly leads to polluting the require cache. Any
@@ -13,6 +14,7 @@ function resetMessaging() {
   delete require.cache[
     require.resolve('../../../app/scripts/extension-bridge/messaging')
   ];
+  messaging = require('../../../app/scripts/extension-bridge/messaging');
 }
 
 /**
@@ -25,29 +27,133 @@ function getSender() {
     require.resolve('../../../app/scripts/extension-bridge/messaging')
   ];
   return { id: extensionId };
-
 }
+
+/**
+ * Get a dummy value for a message to handle from the extension. Includes
+ * properties such that handleExternalMessage() looks for.
+ */
+function getDummyWriteMessage() {
+  return {
+    type: 'write',
+    params: {
+      dataUrl: 'data url',
+      captureUrl: 'capture url',
+      metadata: { meta: 'data' }
+    }
+  };
+}
+
+/**
+ * Proxyquire the messaging module with proxies set as the proxied modules.
+ */
+function proxyquireMessaging(proxies) {
+  messaging = proxyquire(
+    '../../../app/scripts/extension-bridge/messaging',
+    proxies
+  );
+}
+
+test('handleExternalMessage returns false if response undefined', function(t) {
+  var message = getDummyWriteMessage();
+  var sender = getSender();
+
+  proxyquireMessaging({
+    '../persistence/datastore': {
+      addPageToCache: sinon.stub().resolves()
+    }
+  });
+  messaging.getBlobFromDataUrl = sinon.stub();
+
+  var actual = messaging.handleExternalMessage(message, sender);
+
+  t.false(actual);
+  t.end();
+  resetMessaging();
+});
+
+test('handleExternalMessage invokes response on success', function(t) {
+  var message = getDummyWriteMessage();
+  var sender = getSender();
+  var expected = { hi: 'bye' };
+
+  proxyquireMessaging({
+    '../persistence/datastore': {
+      addPageToCache: sinon.stub().resolves()
+    }
+  });
+
+  var createResponseSuccessSpy = sinon.stub().withArgs(expected)
+    .returns(expected);
+  messaging.createResponseSuccess = createResponseSuccessSpy;
+  messaging.getBlobFromDataUrl = sinon.stub();
+
+  var returnValue;
+
+  var callbackFromExtension = function(actual) {
+    t.deepEqual(actual, expected);
+    t.true(returnValue);
+    t.end();
+    resetMessaging();
+  };
+
+  returnValue = messaging.handleExternalMessage(
+    message, sender, callbackFromExtension
+  );
+});
+
+test('handleExternalMessage invokes response on error', function(t) {
+  var message = getDummyWriteMessage();
+  var sender = getSender();
+  var expected = { hi: 'bye' };
+  var errFromDatastore = { msg: 'much wrong' };
+
+  proxyquireMessaging({
+    '../persistence/datastore': {
+      addPageToCache: sinon.stub().rejects(errFromDatastore)
+    }
+  });
+
+  var createResponseErrorSpy = sinon.stub().withArgs(message, errFromDatastore)
+    .returns(expected);
+  messaging.createResponseError = createResponseErrorSpy;
+  messaging.getBlobFromDataUrl = sinon.stub();
+
+  // This will be set below but not checked until our callback is invoked.
+  var returnValue;
+
+  var callbackFromExtension = function(actual) {
+    t.deepEqual(actual, expected);
+    t.true(returnValue);
+    t.end();
+    resetMessaging();
+  };
+
+  returnValue = messaging.handleExternalMessage(
+    message, sender, callbackFromExtension
+  );
+
+});
 
 test('handleExternalMessage adds page to cache for write', function(t) {
   var params = {
     captureUrl: 'www.example.com',
     captureData: 'some day',
-    dataUrl: 'data:base64'
+    dataUrl: 'data:base64',
+    metadata: { foo: 'bar', favicon: 'ugly' }
   };
   var message = {
     type: 'write',
     params: params
   };
-  var addPageToCacheSpy = sinon.spy();
+  var addPageToCacheSpy = sinon.stub().resolves();
 
-  var messaging = proxyquire(
-    '../../../app/scripts/extension-bridge/messaging',
-    {
-      '../persistence/datastore': {
-        addPageToCache: addPageToCacheSpy
-      }
+  proxyquireMessaging({
+    '../persistence/datastore': {
+      addPageToCache: addPageToCacheSpy
     }
-  );
+  });
+
   var blob = {binary: '101s', type: 'mhtml'};
   var getBlobFromDataUrlSpy = sinon.stub().returns(blob);
   messaging.getBlobFromDataUrl = getBlobFromDataUrlSpy;
@@ -59,7 +165,8 @@ test('handleExternalMessage adds page to cache for write', function(t) {
     [
       params.captureUrl,
       params.captureDate,
-      blob
+      blob,
+      params.metadata
     ]
   );
   t.end();
@@ -67,13 +174,11 @@ test('handleExternalMessage adds page to cache for write', function(t) {
 
 test('sendMessageToExtension calls sendMessage', function(t) {
   var sendMessageSpy = sinon.spy();
-  var messaging = proxyquire('../../../app/scripts/extension-bridge/messaging',
-    {
-      '../chrome-apis/runtime': {
-        sendMessage: sendMessageSpy
-      }
+  proxyquireMessaging({
+    '../chrome-apis/runtime': {
+      sendMessage: sendMessageSpy
     }
-  );
+  });
 
   var message = {hello: 'big fella'};
 
@@ -101,4 +206,33 @@ test('sendMessageToOpenUrl sends correct message', function(t) {
   t.deepEqual(sendMessageToExtensionSpy.args[0][0], expectedMessage);
   t.end();
   resetMessaging();
+});
+
+test('createResponseSuccess correct', function(t) {
+  var message = {
+    type: 'write',
+  };
+  var expected = {
+    type: 'write',
+    result: 'success'
+  };
+
+  var actual = messaging.createResponseSuccess(message);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('createResponseError correct', function(t) {
+  var message = { type: 'write' };
+  var err = 'disk too fragmented--things are ruhl crazy over here';
+
+  var expected = {
+    type: 'write',
+    result: 'error',
+    err: err
+  };
+
+  var actual = messaging.createResponseError(message, err);
+  t.deepEqual(actual, expected);
+  t.end();
 });
