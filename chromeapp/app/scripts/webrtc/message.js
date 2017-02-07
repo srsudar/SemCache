db1aa5ae4fed8d57bddc489dd399d1d6ca49cbd4
Sam Sudar <sudar.sam@gmail.com>
2017-02-07 10:18:29 -0800

    Add tests and impl for the webrtc/message.js module

---
+++ chromeapp/app/scripts/webrtc/message.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * Request messages for communicating with peers.
+ *
+ * API:
+ * {
+ *   channelName: /name of the channel to listen to/
+ *   type: { list | file },
+ *   auth: /some object that can later be used for roles permissions/,
+ *   request: {
+ *     / depends on the object itself. could be a file path, eg /
+ *   }
+ * }
+ */
+
+exports.TYPE_LIST = 'list';
+exports.TYPE_FILE = 'file';
+
+/** Valid types of request messages. */
+var VALID_TYPES = [exports.TYPE_LIST, exports.TYPE_FILE];
+
+/**
+ * An increasing suffix of numbers to ensure we create unique channel names.
+ * Although we could use UUIDs, using integers will help with debugging.
+ */
+var CHANNELS_CREATED = 0;
+
+/**
+ * Creates a uuid-based channel name for a message request.
+ *
+ * @returns {String} name for a channel
+ */
+exports.createChannelName = function() {
+  var result = 'responseChannel_' + CHANNELS_CREATED;
+  // Handle rollover.
+  if (CHANNELS_CREATED === Number.MAX_SAFE_INTEGER) {
+    // Will be unlikely to ever happen.
+    console.log('Resetting number of channels to 0, maxed out');
+    CHANNELS_CREATED = 0;
+  } else {
+    CHANNELS_CREATED++;
+  }
+  return result;
+};
+
+exports.createListMessage = function() {
+  return exports.createMessage(exports.TYPE_LIST);
+};
+
+exports.createFileMessage = function(filePath) {
+  var result = exports.createMessage(exports.TYPE_FILE);
+  var request = {};
+  request.path = filePath;
+  result.request = request;
+  return result;
+};
+
+exports.createMessage = function(type) {
+  if (!VALID_TYPES.includes(type)) {
+    throw new Error('Unrecognized message type: ' + type);
+  }
+
+  var result = {};
+
+  result.channelName = exports.createChannelName();
+  result.type = type;
+
+  return result;
+};
+
+exports.isList = function(msg) {
+  return msg.type && msg.type === exports.TYPE_LIST;
+};
+
+exports.isFile = function(msg) {
+  return msg.type && msg.type === exports.TYPE_FILE;
+};
---
+++ chromeapp/test/scripts/webrtc/message-test.js
@@ -0,0 +1,88 @@
+'use strict';
+var test = require('tape');
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+require('sinon-as-promised');
+
+var message = require('../../../app/scripts/webrtc/message');
+
+/**
+ * Manipulating the object directly leads to polluting the require cache. Any
+ * test that modifies the required object should call this method to get a
+ * fresh version
+ */
+function resetMessage() {
+  delete require.cache[
+    require.resolve('../../../app/scripts/webrtc/message')
+  ];
+  message = require('../../../app/scripts/webrtc/message');
+}
+
+test('creates correctly with legal args', function(t) {
+  var typeList = 'list';
+  var typeFile = 'file'; 
+  var channelName = 'channel_123';
+
+  message.createChannelName = sinon.stub().returns(channelName);
+
+  var msg = new message.createMessage(typeList);
+
+  t.equal(msg.type, typeList);
+  t.equal(msg.channelName, channelName);
+
+  var msg = new message.createMessage(typeFile);
+  t.equal(msg.type, typeFile);
+
+  resetMessage();
+  t.end();
+});
+
+test('createMessage throws with invalid type', function(t) {
+  var invalid = function() {
+    new message.createMessage('fake-type');
+  };
+
+  t.throws(invalid, Error);
+  t.end();
+});
+
+test('createListMessage returns type list', function(t) {
+  var actual = message.createListMessage();
+  t.equal(actual.type, message.TYPE_LIST);
+  t.end();
+});
+
+test('createFileMessage returns with request information', function(t) {
+  var path = 'path/to/file.mhtml';
+  var actual = message.createFileMessage(path);
+
+  t.equal(actual.type, message.TYPE_FILE);
+  t.equal(actual.request.path, path);
+  t.end();
+});
+
+test('isList correct', function(t) {
+  var obj = {};
+
+  t.false(message.isList(obj));
+  obj.type = 'fake';
+  t.false(message.isList(obj));
+
+  obj.type = message.TYPE_LIST;
+  t.true(message.isList(obj));
+
+  t.end();
+});
+
+test('isFile correct', function(t) {
+  var obj = {};
+
+  t.false(message.isList(obj));
+  obj.type = 'fake';
+  t.false(message.isList(obj));
+
+  obj.type = message.TYPE_FILE;
+  t.true(message.isFile(obj));
+
+  t.end();
+});
