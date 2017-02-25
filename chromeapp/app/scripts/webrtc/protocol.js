6db5c2ac5480f69f5dc68326f4f599f6b3b0a12e
Sam Sudar <sudar.sam@gmail.com>
2017-02-24 17:31:44 -0800

    Add protocol module
    With our naive implementation of sending data over chunking-channel, we
    had no way to indicate things like a server error. This adds as thin
    frame to each message, allowing content to be sent with a header,
    essentially. This is not yet plugged into chunking-channel.

---
+++ chromeapp/app/scripts/webrtc/protocol.js
@@ -0,0 +1,173 @@
+'use strict';
+
+/**
+ * Protocol for transmitting data via WebRTC data channel.
+ *
+ * This exists to solve the problem that we ideally would like to transmit both
+ * metadata control information (e.g. "uh oh, there's been a server error"),
+ * and the data chunks themselves. We could have a control channel and a data
+ * channel, but this requires coordination of two channels, as well as an
+ * additional channel resource.
+ *
+ * We also cannot simply pass an ArrayBuffer as a member of a JSON message.
+ * Instead, we are going to going to include header information in the message
+ * itself.
+ *
+ * This class assists in creating and reading those messages.
+ */
+
+/** The number of bytes used to indicate the length of the header. */
+var NUM_BYTES_HEADER_LENGTH = 4;
+
+/**
+ * These status codes are based on HTTP status codes, and are added as
+ * necessary.
+ */
+exports.STATUS_CODES = {
+  error: 500,
+  ok: 200
+};
+
+/**
+ * @constructor
+ * 
+ * @param {JSON} header
+ * @param {Buffer} buff
+ */
+exports.ProtocolMessage = function ProtocolMessage(header, buff) {
+  if (!(this instanceof ProtocolMessage)) {
+    throw new Error('ProtocolMessage must be called with new');
+  }
+  this.header = header;
+
+  // Distinguishing between 0 length Buffers and null is problematic for
+  // serialization. To ensure we have consistent behavior, we'll default to an
+  // empty buffer.
+  if (!buff) {
+    buff = Buffer.alloc(0);
+  }
+  this.data = buff;
+};
+
+exports.ProtocolMessage.prototype.isOk = function() {
+  var statusCode = this.getStatusCode();
+  return statusCode === exports.STATUS_CODES.ok;
+};
+
+exports.ProtocolMessage.prototype.isError = function() {
+  var statusCode = this.getStatusCode();
+  return statusCode === exports.STATUS_CODES.error;
+};
+
+exports.ProtocolMessage.prototype.getHeader = function() {
+  return this.header;
+};
+
+exports.ProtocolMessage.prototype.getData = function() {
+  return this.data;
+};
+
+exports.ProtocolMessage.prototype.getStatusCode = function() {
+  if (!this.header) {
+    return null;
+  }
+  if (!this.header.status) {
+    return null;
+  }
+  return this.header.status;
+};
+
+/**
+ * Get this ProtocolMessage as a Buffer, serializing the method. This Buffer
+ * can then be deserialized using the from() method.
+ *
+ * @return {Buffer}
+ */
+exports.ProtocolMessage.prototype.asBuffer = function() {
+  /*
+   * The data structure is outlined as follows, but is not part of the public
+   * API. The first 4 bytes correspond to an integer. This integer denotes the
+   * length of the JSON header information. All remaining bytes are data bytes.
+   */
+  var metadataLength = NUM_BYTES_HEADER_LENGTH;
+  var headerStr = '';
+  var headerLength = 0;
+  if (this.header) {
+    headerStr = JSON.stringify(this.header);
+    headerLength = headerStr.length;
+    metadataLength += headerLength;
+  }
+
+  var metadataBuff = Buffer.alloc(metadataLength);
+
+  var offset = 0;
+  metadataBuff.writeUInt32BE(headerLength);
+  offset += NUM_BYTES_HEADER_LENGTH;
+
+  metadataBuff.write(headerStr, offset, headerLength);
+
+  var buffsToJoin = [ metadataBuff ];
+  if (this.data) {
+    buffsToJoin.push(this.data);
+  }
+
+  var result = Buffer.concat(buffsToJoin);
+  return result;
+};
+
+/**
+ * Recover a ProtocolMessage from a Buffer.
+ *
+ * @param {Buffer} buff
+ *
+ * @return {ProtocolMessage}
+ */
+exports.from = function(buff) {
+  var headerLength = buff.readUInt32BE();
+  var offset = NUM_BYTES_HEADER_LENGTH;
+  var headerStr = buff.toString('utf8', offset, offset + headerLength);
+  offset += headerLength;
+
+  var header = null;
+  if (headerLength > 0) {
+    header = JSON.parse(headerStr);
+  }
+  var data = buff.slice(offset, buff.length);
+
+  var result = new exports.ProtocolMessage(header, data);
+  return result;
+};
+
+/**
+ * Create a rudimentary header object.
+ *
+ * @param {integer} status
+ *
+ * @return {JSON}
+ */
+exports.createHeader = function(status) {
+  return {
+    status: status
+  };
+};
+
+/**
+ * @param {Buffer} buff
+ *
+ * @return {ProtocolMessage}
+ */
+exports.createSuccessMessage = function(buff) {
+  var header = exports.createHeader(exports.STATUS_CODES.ok);
+  return new exports.ProtocolMessage(header, buff);
+};
+
+/**
+ * @param {any} reason the reason for the error
+ *
+ * @return {ProtocolMessage}
+ */
+exports.createErrorMessage = function(reason) {
+  var header = exports.createHeader(exports.STATUS_CODES.error);
+  header.message = reason;
+  return new exports.ProtocolMessage(header, null);
+};
---
+++ chromeapp/test/scripts/webrtc/protocol-test.js
@@ -0,0 +1,159 @@
+'use strict';
+var Buffer = require('buffer').Buffer;
+var test = require('tape');
+require('sinon-as-promised');
+
+var protocol = require('../../../app/scripts/webrtc/protocol');
+
+/**
+ * Manipulating the object directly leads to polluting the require cache. Any
+ * test that modifies the required object should call this method to get a
+ * fresh version
+ */
+function resetProtocol() {
+  delete require.cache[
+    require.resolve('../../../app/scripts/webrtc/protocol')
+  ];
+  protocol = require('../../../app/scripts/webrtc/protocol');
+}
+
+function end(t) {
+  if (!t) { throw new Error('You forgot to pass t to end'); }
+  t.end();
+  resetProtocol();
+}
+
+/**
+ * Assert that the msg serializes and deserializes correctly. Performs the
+ * assertion and ends the test.
+ *
+ * @param {ProtocolMessage} msg
+ */
+function serializeDeserializeHelper(msg, t) {
+  var serialized = msg.asBuffer();
+  var recovered = protocol.from(serialized);
+
+  t.notEqual(null, serialized);
+  t.notEqual(undefined, serialized);
+  t.deepEqual(recovered, msg);
+  end(t);
+}
+
+test('getters return values', function(t) {
+  var header = { foo: 'bar' };
+  var buff = 'buffer';
+
+  var msg = new protocol.ProtocolMessage(header, buff);
+
+  t.equal(msg.getHeader(), header);
+  t.equal(msg.getData(), buff);
+  end(t);
+});
+
+test('createSuccessMessage correct', function(t) {
+  var buff = 'buffer';
+  var msg = protocol.createSuccessMessage(buff);
+  t.equal(msg.getData(), buff);
+  t.equal(msg.getHeader().status, protocol.STATUS_CODES.ok);
+  end(t);
+});
+
+test('createErorMessage correct', function(t) {
+  var reason = 'something went wrong';
+  var msg = protocol.createErrorMessage(reason);
+  t.deepEqual(msg.getData(), Buffer.alloc(0));
+  t.equal(msg.getHeader().status, protocol.STATUS_CODES.error);
+  t.equal(msg.getHeader().message, reason);
+  end(t);
+});
+
+test('createHeader sets status', function(t) {
+  var status = 123;
+  var expected = { status: status };
+  var actual = protocol.createHeader(status);
+  t.deepEqual(actual, expected);
+  end(t);
+});
+
+test('isOk true for status OK', function(t) {
+  var okMsg = protocol.createSuccessMessage();
+  t.true(okMsg.isOk());
+  end(t);
+});
+
+test('isOk false for status error', function(t) {
+  var errorMsg = protocol.createErrorMessage();
+  t.false(errorMsg.isOk());
+  end(t);
+});
+
+test('getStatusCode null for no header', function(t) {
+  var msg = new protocol.ProtocolMessage(null, null);
+  var actual = msg.getStatusCode();
+  t.equal(actual, null);
+  end(t);
+});
+
+test('getStatusCode null for no status in header', function(t) {
+  var msg = new protocol.ProtocolMessage({}, null);
+  var actual = msg.getStatusCode();
+  t.equal(actual, null);
+  end(t);
+});
+
+test('getStatusCode returns status from header', function(t) {
+  var msg = protocol.createSuccessMessage('buffer');
+  t.equal(msg.getStatusCode(), protocol.STATUS_CODES.ok);
+  end(t);
+});
+
+test('isError true for status error', function(t) {
+  var errorMsg = protocol.createErrorMessage();
+  t.true(errorMsg.isError());
+  end(t);
+});
+
+test('isError true for status ok', function(t) {
+  var okMsg = protocol.createSuccessMessage();
+  t.false(okMsg.isError());
+  end(t);
+});
+
+test('serialization correct base case', function(t) {
+  // The base case is a header with values in the header and a buffer.
+  var buff = Buffer.from('hello there this is fine');
+  var header = {
+    status: 200,
+    metadata: { something: 'else' }
+  };
+  var msg = new protocol.ProtocolMessage(header, buff);
+
+  serializeDeserializeHelper(msg, t);
+});
+
+test('serialization correct no header', function(t) {
+  var buff = Buffer.from('nope nope nope');
+  var msg = new protocol.ProtocolMessage(null, buff);
+
+  serializeDeserializeHelper(msg, t);
+});
+
+test('serialization correct no data', function(t) {
+  var header = {
+    status: 500,
+    msg: 'much trouble is occurring'
+  };
+
+  var msg = new protocol.ProtocolMessage(header, null);
+
+  serializeDeserializeHelper(msg, t);
+});
+
+test('serialization correct empty buffer', function(t) {
+  var buff = Buffer.from('');
+  var header = { status: 200 };
+
+  var msg = new protocol.ProtocolMessage(header, buff);
+
+  serializeDeserializeHelper(msg, t);
+});
